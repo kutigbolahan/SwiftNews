df375513c4c7523e2fbd7bd2ce081042bdf3b1b9
Kuti Gbolahan <kutigbolahan.ab@gmail.com>
2022-08-23 12:41:05 +0100

    Initial Commit

---
+++ Swiftnews/SwiftnewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftnewsApp.swift
+//  Swiftnews
+//
+//  Created by GB on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftnewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
