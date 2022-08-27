23f121ffde22bc35bf1920aaba0465245e27cbb4
Kuti Gbolahan <kutigbolahan.ab@gmail.com>
2022-08-27 20:23:33 +0100

    Buildt a component

--- Swiftnews/Model/NewsResponse.swift
+++ Swiftnews/Model/NewsResponse.swift
@@ -17,14 +17,26 @@ struct NewsResponse: Codable {
 struct Article: Codable,Identifiable {
     let id = UUID()
     let author: String?
-    let url: String
-    let source, title, articleDescription: String
-    let image: String
-    let date: Date
+    let url: String?
+    let source, title, articleDescription: String?
+    let image: String?
+    let date: Date?
 
     enum CodingKeys: String, CodingKey {
         case author, url, source, title
         case articleDescription = "description"
         case image, date
+    }
+}
+
+extension Article{
+    static var dummyData: Article {
+        .init(author: "John Wagner, Mariana Alfaro",
+              url: "https://www.washingtonpost.com/politics/2022/08/24/biden-student-debt-ukraine-aid/",
+              source: "The Washington Post",
+              title: "Post Politics Now: Biden returning to Washington with news on Ukraine aid, possibly student debt - The Washington Post",
+              articleDescription: "The president is scheduled to be back at the White House on Wednesday after vacationing in Delaware. His administration is expected to make news on domestic and international issues.",
+              image: "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/ZP5THMAXM4I63OMYWKVWR5MENA.jpg&w=1440",
+              date: Date())
     }
 }
---
+++ Swiftnews/Views/ArticleView.swift
@@ -0,0 +1,56 @@
+//
+//  ArticleView.swift
+//  Swiftnews
+//
+//  Created by GB on 8/24/22.
+//
+
+import SwiftUI
+import URLImage
+
+
+
+
+//using optional chanining: to see if we should use the url to display an image or use a place holder
+struct ArticleView: View {
+    let article : Article
+    var body: some View {
+        HStack{
+            if let imgUrl = article.image,
+               let url = URL(string: imgUrl){
+                URLImage(url,
+//                         options: URLImageOptions(
+//                            identifier: article.id.uuidString,
+//                            cachePolicy:.returnStoreElseLoad(downloadDelay:0.25, cacheDelay: nil)
+//
+//                         ),
+                         failure: { error, _ in
+                   
+                    PlaceHolderImageView()
+                },
+                         content: {
+                    image in
+                    image.resizable().aspectRatio(contentMode: .fill)
+                }).frame(width: 100, height: 100).cornerRadius(10)
+            }else{
+                PlaceHolderImageView()
+            }
+            VStack(alignment: .leading, spacing: 4 ){
+                Text(article.title ?? " ").foregroundColor(.black).font(.system(size: 18, weight: .semibold))
+                Text(article.source ?? "N/A").foregroundColor(.gray).font(.footnote)
+            }
+        }
+    }
+}
+
+    struct PlaceHolderImageView: View{
+        var body: some View{
+            Image(systemName: "photo.fill").foregroundColor(.white).background(Color.gray).frame(width: 100, height: 100)
+        }
+    }
+
+struct ArticleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleView(article: Article.dummyData).previewLayout(.sizeThatFits)
+    }
+}
