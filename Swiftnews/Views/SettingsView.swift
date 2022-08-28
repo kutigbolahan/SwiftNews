da02cbaa50cee3f650ee5e34fa259b2694b1d659
Kuti Gbolahan <kutigbolahan.ab@gmail.com>
2022-08-28 13:39:35 +0100

    settings page added

--- Swiftnews/Views/SettingsView.swift
+++ Swiftnews/Views/SettingsView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            Form {
+                Section(header: Text("Display"), footer: Text("System settings will override Dark Mode and use the current device theme")){
+                    Toggle(isOn: .constant(false), label: {
+                        Text("Dark Mode")
+                    })
+                    
+                    Toggle(isOn: .constant(true), label: {
+                        Text("use system settings")
+                    })
+                }
+                Section{
+                    Label("Follow me on twitter @kutigbolahan", systemImage: "link")
+                }.foregroundColor(.blue).font(.system(size:16,weight:.semibold))
+          
+            }
+            .navigationTitle("Settings")
+        }
     }
 }
 
